a19324b122ab4570215576c9883ce6ef41382d4e
Todd Scharien <todd.scharien@gmail.com>
2020-02-16 19:01:49 -0800

    Rename "funshine" to "Funshine"

--- funshine/Pages/Privacy.cshtml.cs
+++ funshine/Pages/Privacy.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace funshine.Pages
+namespace Funshine.Pages
 {
     public class PrivacyModel : PageModel
     {
--- funshine/Startup.cs
+++ funshine/Startup.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace funshine
+namespace Funshine
 {
     public class Startup
     {
